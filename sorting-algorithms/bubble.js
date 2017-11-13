00dba1f897373f7d9bc4701d3c7d963c64331c1e
Elsha Kwee <elsha.e.k@gmail.com>
2017-11-13 20:27:33 +0700

    Bubble sort JS

--- sorting-algorithms/bubble.js
+++ sorting-algorithms/bubble.js
@@ -19,3 +19,41 @@ Variants:
 (https://en.wikipedia.org/wiki/Cocktail_sort)
 
 */
+// Solution 1
+
+function swap(arr, i, j) {
+  var temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+// function bubbleSort(arr) {
+//   for (var i = 0; i < arr.length; i++) {
+//     for (var j = 1; j < arr.length; j++) {
+//       if (arr[j - 1] > arr[j]) {
+//         swap(arr, j - 1, j);
+//       }
+//     }
+//   }
+//   return arr;
+// }
+
+// Solution 2
+
+function bubbleSort(arr) {
+  var swapped;
+  do {
+    swapped = false;
+    for (var i = 1; i < arr.length; i++) {
+      if (arr[i] < arr[i-1]) {
+        swap(arr, i, i-1);
+        swapped = true;
+      }
+    }
+  } while(swapped);
+  return arr;
+}
+
+var givenArr = [4, 6, 324, 345, 78, 34, 3874, 37, 78, 12, 48, 52];
+
+console.log(bubbleSort(givenArr));
